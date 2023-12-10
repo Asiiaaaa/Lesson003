f63be8772c222550f80f65df39410f3b0bd4f6fc
Asiiaaaa <anastasiya23091988@gmail.com>
2023-12-11 00:20:04 +0300

    add seminar and homework

---
+++ HomeWork003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2.  Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+Console.Write("Введите кол-во элементов: ");
+Random random = new Random();
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+            int count = 0;
+
+            foreach (int num in array)
+            {
+                if (num % 2 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Количество четных чисел в массиве: " + count);
+        
+    
---
+++ SEMINAR003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write ("Введите количество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+
+for(int i =0; i< array.Length; i++)
+    array[i] = new Random().Next(0,11);
+
+for(int i =0; i< array.Length; i++)
+    Console.Write(array[i] + " ");
+
+Console.WriteLine();
+Console.WriteLine(string.Join (" , ", array));
